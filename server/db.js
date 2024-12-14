e96d28dc71f0d7103438f56266a8cae4c5a73c45
Anbarasan Subramani <161632074+S-Anbu@users.noreply.github.com>
2024-12-14 21:45:23 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const dbConnect=async()=>{
     try {
-        await mongoose.connect('mongodb+srv://anbu:123@cluster0.zo2og.mongodb.net/register?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect('mongodb+srv://YOUR_USERNAME:YOUR_PASSWORD@cluster0.zo2og.mongodb.net/register?retryWrites=true&w=majority&appName=Cluster0')
         console.log(`mongodb connected successfully`);
     } catch (error) {
         console.log(`mongodb connection error ${error} `);      
